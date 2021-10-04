bd98f83b2d8b58e37dd48ec9f4c10bcbf47b3d92
ChrisHursty <chris@iamchrishurst.com>
2021-10-04 16:47:06 -0400

    Updated logo, header, and member login

--- header.php
+++ header.php
@@ -1,9 +1,7 @@
 <?php
 /**
  * The header for our theme
- *
  * Displays all of the <head> section and everything up till <div id="content">
- *
  * @package Understrap
  */
 
@@ -20,27 +18,27 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
 <?php do_action( 'wp_body_open' ); ?>
 <div class="site" id="page">
-
 	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar">
-
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
-		<div class="container">
+		<div class="container header-section">
 				<div class="row">
 				<!-- Your site title as branding in the menu -->
-					<div>
+					<div class="col-2 logo">
 						<?php the_custom_logo(); ?>
 					</div>
-					<div>
+					<div class="col-6 title">
 						<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
 						<h2><?php bloginfo( 'description' ); ?></h2>
 					</div>
+					<div class="col-4">
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/member-login-icon.png" alt="Member Login Icon">
+						<h5>SSC Member Login</h5>
+					</div>
 				</div>
-				
 			</div>
 			<!-- .container -->
 		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
@@ -48,7 +46,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<h2 id="main-nav-label" class="sr-only">
 				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
 			</h2>
-			<div class="container">
+			<div class="container-fluid">
 				<div class="row">
 				
 					<div class="col">
