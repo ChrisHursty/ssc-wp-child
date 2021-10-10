f371b6161022c3d58212963a89dd1e2fbbb9f95b
ChrisHursty <chris@iamchrishurst.com>
2021-10-10 09:04:06 -0400

    Added global scss for all templates

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 <div class="container-fluid">
     <div class="row">
-        <div class="hero-image-container" style="background-image: url('<?php if (get_theme_mod( 'home_hero_img' )) : echo get_theme_mod( 'home_hero_img'); else: echo get_stylesheet_directory_uri() . '/img/home-page-hero.jpg'; endif; ?>');">
+        <div class="hero-image-container" style="background-image: url('<?php if (get_theme_mod( 'home_hero_img' )) : echo get_theme_mod( 'home_hero_img' ); else: echo get_stylesheet_directory_uri() . '/img/home-page-hero.jpg'; endif; ?>');">
         </div>
     </div>
 </div>
